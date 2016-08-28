3321d80456138651e9636c9d6bdf45d391569094
ankur <ankur@ankurs-mac.local>
2016-08-29 00:33:49 +0530

    Initial Commit

---
+++ CountStringTests/CountStringTests.m
@@ -0,0 +1,39 @@
+//
+//  CountStringTests.m
+//  CountStringTests
+//
+//  Created by ankur on 29/08/16.
+//  Copyright © 2016 ankur. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface CountStringTests : XCTestCase
+
+@end
+
+@implementation CountStringTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
